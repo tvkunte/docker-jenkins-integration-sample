f3cb48acbb0a63f7619c8517b27f1db98fab907b
tvkunte <tvkunte@gmail.com>
2022-05-13 18:13:57 +0530

    updated statement

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -3,7 +3,7 @@ public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi, docker demo here");
+		System.out.println("Hi, docker demo here - new");
 
 	}
 
